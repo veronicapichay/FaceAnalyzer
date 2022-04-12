aec5f9ecc54d8a37c6d9e058e4487b30e12a1d43
Veronica Pichay <veronicapichay@outlook.com>
2022-04-11 19:04:48 -0700

    Add project files.

---
+++ FaceAnalyzer/AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace FaceAnalyzer
+{
+    //singleton that keeps api key from secret in my local machine
+    public class AppConfiguration
+    {
+        public string ApiKey { get; set; }
+
+
+
+    }
+}
